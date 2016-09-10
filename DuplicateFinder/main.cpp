ee110d6f9963c41ca65b04a937e3631d46b8b1a3
Jack <jack@hackz.homenet.telecomitalia.it>
2016-09-10 04:59:23 +0200

    Initial Commit

---
+++ DuplicateFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DuplicateFinder
+//
+//  Created by Jack on 10/09/16.
+//  Copyright © 2016 Jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
